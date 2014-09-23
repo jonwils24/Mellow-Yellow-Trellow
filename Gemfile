b0f072f4c6cef7c5497bd548bf686cbe4ba9e2af
Jonathan Wilson <wilsonwjonathan@gmail.com>
2014-09-23 09:54:10 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.5'
+gem 'pg'
+gem 'sass-rails', '~> 4.0.2'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+gem 'bcrypt', '~> 3.1.7'
+gem 'newrelic_rpm', '~> 3.9.4.245'
+
+# Use unicorn as the app server
+# gem 'unicorn'
+
+# Use Capistrano for deployment
+# gem 'capistrano', group: :development
+
+# Use debugger
+# gem 'debugger', group: [:development, :test]
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TrelloClone::Application.config.secret_key_base = '8b1868aa71ef5528d375fddb3e971afc8cecf7bfc5511abb93f2256542c9fd92a3cb868aeb9555f202bf28614b6ee5b08ee9a6a244e053893d91495ba91278ab'
